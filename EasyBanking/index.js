a87d6b82d835486095ae7d1c158c8ac62a03f4a1
annabelowigho <113777360+annaelowigho@users.noreply.github.com>
2023-07-31 14:34:34 +0100

    Add files via upload

---
+++ EasyBanking/index.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById(menu)
+function showMenu(){
+    onclick = 
+            navLinks.style.right = "0"
+        }
+        function hideMenu(){
+            navLinks.style.right = "-300px"
+        }
